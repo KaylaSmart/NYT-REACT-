bb81908273ca93d661b3f892e6014349c9a0971d
Kayla Smart <kaylasmart.wma@gmail.com>
2020-03-04 21:15:26 -0500

    refactor

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -1,3 +1,4 @@
-export * from "./Input";
-export * from "./TextArea";
-export * from "./FormBtn";
+export { default } from "./Form";
+// export * from "./Input";
+// export * from "./TextArea";
+// export * from "./FormBtn";
